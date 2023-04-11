56c3fd5c1185503a578b82a07b67489f10621157
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-11 15:38:44 +0000

    deploy: 2ca6d38cbbab500827035591cd2929026238c257

--- code.js
+++ code.js
@@ -1,3 +1,53 @@
 
 // Your Code Here.
-//
+
+let player1 = "R";
+let player2 = "B";
+let currPlayer = player1;
+let playerTurn = document.getElementsByClassName("turnTracker");
+let startButton = document.getElementById("startGame")
+let message = document.getElementsByClassName("message")
+let startScreen = document.getElementsByClassName("startScreen")
+
+let board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+];
+//Function to check to see if there is four in a row
+let checkArray = function (array) {
+    let result = false;
+    let discCount = 0;
+    array.forEach((element, index) => {
+        if(element === currPlayer){
+            discCount += 1;
+            if(discCount === 4){
+                result = true;
+            }
+        }
+        else {
+            discCount = 0;
+        }
+    });
+    return result
+}
+
+//Check for game over
+
+let gameOver = function(){
+    let finalCount = 0;
+    for(let innerArray of board){
+        if (innerArray.every((value) => value != 0)) {
+            finalCount += 1;
+        } else {
+            return false
+        }
+    }
+    if(finalCount === 6){
+        message.alert("Game Over!");
+        startScreen.classList.remove("hide")
+    }
+};
